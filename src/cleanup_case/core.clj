6633d511863f47224698928ee9ba151811898869
Paul Gowder <paul.gowder@gmail.com>
2017-02-11 20:14:43 -0600

    clean body

--- src/cleanup_case/core.clj
+++ src/cleanup_case/core.clj
@@ -17,6 +17,8 @@
 
   If not, I can probably find the anchor for footnote numbers just by searching for the word Footnotes in a table and grabbing the class before that via regex, then incrementing for text.
 
+  label for opinion to split on: <p class=\"p17\"><b>Opinion</b></p>
+
 
   "
   (:require
@@ -28,14 +30,30 @@
 
 (def working-file (atom {}))
 
-(defn mark-footnote-refs [case-string]
-  (str/replace case-string #"<span class=\"s2\"><sup>(\d*)<\/sup><\/span>" "[note $1]"))
+(defn split-on-opinion [page-html]
+  (let [[front body] (str/split page-html #"<p class=\"p17\"><b>Opinion</b></p>" 2)]
+  {:front front :body body}))
 
-(defn tree-from-file [filename]
-  (html/html-resource
-   (java.io.StringReader.
-    (mark-footnote-refs
-     (slurp filename)))))
+(defn pre-clean [case-string]
+  (let [s1 (str/replace case-string #"<span class=\"s2\"><sup>(\d*)<\/sup><\/span>" "[note $1]") ; note markers
+        s2 (str/replace s1 #"p18" "p10") ; make opinion author labels real paragraphs
+        s3 (str/replace s2 #"p21" "p10") ; make opinion author labels real paragraphs
+        final-string (str/replace s3 #"p11" "p10")] ; make opinion author labels real paragraphs
+   final-string))
+
+
+(defn make-tree [hstring]
+  (-> hstring html/html-snippet html/html-resource))
+
+(defn trees-from-file [filename]
+  (let [{:keys [front body]} (split-on-opinion (pre-clean (slurp filename)))]
+    {:front (make-tree front) :body (make-tree body)}))
+
+;; (defn tree-from-file [filename]
+;;   (html/html-resource
+;;    (java.io.StringReader.
+;;     (mark-footnote-refs
+;;      (slurp filename)))))
 
 (defn selectvec
   "selector is a vector"
@@ -63,17 +81,21 @@
 ;; paragraphs
 
 (defn clean-paragraph [paragraph]
-  (let [cleaned-paragraph (str/replace paragraph #"\s?\*+\d+\s?" " ")]
-    cleaned-paragraph)) 
+  (let [s1 (str/replace paragraph #"\s?\*+\d+\s?" " ")
+        cleaned-paragraph (str/replace s1 #"\[\d+\]\s?" "")]
+    cleaned-paragraph))
 
 (defn remove-bad-grafs [paragraph-vec]
   (remove str/blank? paragraph-vec))
+;; this doesn't actually seem to be doing anything, but I've hacked around it so whev.
 
 (defn extract-paragraphs [tree]
   (let [raw-ps (selectvec tree [:p.p10])
         filtered-ps (remove-bad-grafs raw-ps)
         cleaned-ps (mapv clean-paragraph filtered-ps)]
-    (str/join "\n\n" cleaned-ps)))
+    (str/join "\n" cleaned-ps)))
+
+
 
 ;;; main for testing and stuff
 
@@ -81,7 +103,7 @@
   "in experimenting"
   [& args]
   (do
-    (reset! working-file (tree-from-file "nfiborig.html"))
-    (let [tree @working-file]
+    (reset! working-file (trees-from-file "nfiborig.html"))
+    (let [tree (:body @working-file)]
       (spit "test-paragraphs.txt" (extract-paragraphs tree))
   )))

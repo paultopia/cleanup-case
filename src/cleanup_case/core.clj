dc844706f141a8565c1e2f6e6d790cde068371a9
Paul Gowder <paul.gowder@gmail.com>
2017-02-11 21:58:37 -0600

    properly generic on nfib

--- src/cleanup_case/core.clj
+++ src/cleanup_case/core.clj
@@ -1,26 +1,4 @@
 (ns cleanup-case.core
-  "Being lazy and putting everything in this namespace.
-  Selectors of relevance in html (extracted from rtf by textutil on osx)
-
-  - text: <p class=\"p10\">
-
-  - footnote anchors in text: <span class=\"s2\"><sup>14</sup></span>
-
-  - name, citation and court in p classes p3-p8
-
-  - headers: p19.  (I don't know if this is consistent, or will depend on doc)
-  - footnote references: p23, in a link and a <sup>
-
-  - footnote text: p24
-
-  Really need to double-check and make sure this works for all cases
-
-  If not, I can probably find the anchor for footnote numbers just by searching for the word Footnotes in a table and grabbing the class before that via regex, then incrementing for text.
-
-  label for opinion to split on: <p class=\"p17\"><b>Opinion</b></p>
-
-
-  "
   (:require
    [net.cgrand.enlive-html :as html]
    [clojure.string :as str])
@@ -36,14 +14,8 @@
     opinion))
 
 (defn pre-clean [case-string]
-  (let [s1 (str/replace case-string #"<span class=\"s2\"><sup>(\d*)<\/sup><\/span>" "[note $1]") ; note markers
-        s2 (str/replace s1 #"p18" "p10") 
-        s3 (str/replace s2 #"p21" "p10")
-        s4 (str/replace s3 #"p9" "p10")
-        final-string (str/replace s4 #"p11" "p10")]
-
-   final-string))
-
+  (let [cleaned (str/replace case-string #"<span class=\"s2\"><sup>(\d*)<\/sup><\/span>" "[note $1]")]
+   cleaned))
 
 (defn make-tree [hstring]
   (-> hstring html/html-snippet html/html-resource))
@@ -76,7 +48,7 @@
 ;; this doesn't actually seem to be doing anything, but I've hacked around it so whev.
 
 (defn extract-paragraphs [tree]
-  (let [raw-ps (selectvec tree [:p.p10])
+  (let [raw-ps (mapv html/text (html/select tree [:p]))
         filtered-ps (remove-bad-grafs raw-ps)
         cleaned-ps (mapv clean-paragraph filtered-ps)]
     (str/join "\n" cleaned-ps)))

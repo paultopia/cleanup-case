d8a3c8955cdfe29baee5371378959d61b359e6e1
Paul Gowder <paul.gowder@gmail.com>
2017-12-20 13:17:41 -0600

    weird behavior, correct and incorrect both working

--- src/cleanup_case/core.clj
+++ src/cleanup_case/core.clj
@@ -15,7 +15,21 @@
         date (elements->text (.select soup ".co_date"))]
     (str title "\n\n" cite "\n\n" court "\n\n" date)))
 
+(defn correct-remove-starpages [soup]
+  (let [newsoup soup
+        starpages (.select newsoup ".co_starPage")]
+    (.remove starpages)
+    newsoup))
+
+(defn incorrect-remove-starpages [soup]
+  (let [starpages (.select soup ".co_starPage")]
+    (.remove starpages)
+    soup))
 
 (defn -main
   [filename]
-  (println (get-header-info (file->soup filename))))
+  (do
+    (spit "testbad.html" (.toString (incorrect-remove-starpages (file->soup filename))))
+    (spit "testgood.html" (.toString (correct-remove-starpages (file->soup filename))))
+    )
+  )
